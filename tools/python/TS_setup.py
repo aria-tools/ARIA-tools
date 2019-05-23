ec8bc24fde8f70fe87e7142c8240c24f7f98501b
ehavazli <emre.havazli@gmail.com>
2019-05-22 21:20:17 -0700

    Eh (#24)
    * removed TkAgg
    
    * added option to create intereferogram and coherence stack VRTs
     - metadata includes wavelenght and calculates mid-frame UTC Time
    
    * - extract all the products for unwrapped phase, coherence and perpendicular baseline layers.
    - extract only the first product for incidence angle and look angle layers.
    
    * requirements.txt added
    README.md updated
    TS_setup.py extraction of products separated
    
    * - generateStack is a function now
    - metadata conventions changed
    - connectedComponents added to be extracted (placeholder for now)
    - connCompStack.vrt created in stack folder
    
    * - changed installation instructions to levearge anaconda installation
    - added requiremets.txt file and made appropriate changes in the text
    
    * removed leftover notation
    
    * - add workdir instead of inps.workdir in generateStack function
    - remove connectedComponents from extract product
    - bbox if clause removed from size calculation

--- tools/python/TS_setup.py
+++ tools/python/TS_setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-# Author: Simran Sangha & David Bekaert
+# Author: Simran Sangha & David Bekaert & Emre Havazli
 # Copyright 2019, by the California Institute of Technology. ALL RIGHTS
 # RESERVED. United States Government Sponsorship acknowledged.
 #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import os
 import sys
+import glob
 import numpy as np
+from datetime import datetime, time
 
 from osgeo import gdal
 gdal.UseExceptions()
@@ -32,15 +34,13 @@ def createParser():
 
     import argparse
     parser = argparse.ArgumentParser(description='Get DEM')
-    parser.add_argument('-f', '--file', dest='imgfile', type=str,
-            required=True, help='ARIA file')
+    parser.add_argument('-f', '--file', dest='imgfile', type=str, required=True, help='ARIA file')
     parser.add_argument('-w', '--workdir', dest='workdir', default='./', help='Specify directory to deposit all outputs. Default is local directory where script is launched.')
-    parser.add_argument('-d', '--demfile', dest='demfile', type=str,
-            default=None, help='DEM file. To download new DEM, specify "Download".')
-    parser.add_argument('-p', '--projection', dest='projection', default='WGS84', type=str,
-            help='projection for DEM. By default WGS84.')
+    parser.add_argument('-d', '--demfile', dest='demfile', type=str, default=None, help='DEM file. To download new DEM, specify "Download".')
+    parser.add_argument('-p', '--projection', dest='projection', default='WGS84', type=str, help='projection for DEM. By default WGS84.')
     parser.add_argument('-b', '--bbox', dest='bbox', type=str, default=None, help="Provide either valid shapefile or Lat/Lon Bounding SNWE. -- Example : '19 20 -99.5 -98.5'")
     parser.add_argument('-m', '--mask', dest='mask', type=str, default=None, help="Provide valid mask file.")
+    parser.add_argument('-s', '--stack', action='store_true', dest='stack', help="If turned on, creates vrt files of stacks that can be used for time series processing")
     parser.add_argument('-croptounion', '--croptounion', action='store_true', dest='croptounion', help="If turned on, IFGs cropped to bounds based off of union and bbox (if specified). Program defaults to crop all IFGs to bounds based off of common intersection and bbox (if specified).")
     parser.add_argument('-verbose', '--verbose', action='store_true', dest='verbose', help="Toggle verbose mode on.")
 
@@ -49,6 +49,121 @@ def createParser():
 def cmdLineParse(iargs = None):
     parser = createParser()
     return parser.parse_args(args=iargs)
+
+def generateStack(inputFiles,outputFileName,workdir='./'):
+    ##Mid-frame UTC times to dictionary
+    f = list(np.sort(standardproduct_info.files))
+    utcDict = {}
+    dates= []
+    times = []
+    for i in range(0,len(f)):
+        if f[i].split('-')[6] in dates:
+            times.append(f[i].split('-')[7])
+            utcDict[dates[-1]] = times
+        else:
+            times = []
+            dates.append(f[i].split('-')[6])
+            times.append(f[i].split('-')[7])
+            utcDict[f[i].split('-')[6]] = times
+
+    UTC_time = {}
+    for key, value in utcDict.items():
+        tFirst = datetime.strptime(value[0],'%H%M%S')
+        tLast = datetime.strptime(value[-1],'%H%M%S')
+        tDelta = (tLast - tFirst)/2
+        tMid = tFirst+tDelta
+        UTC_time[key] = datetime.strftime(tMid,'%H%M%S')
+
+    ###Set up single stack file
+    if not os.path.exists(os.path.join(workdir,'stack')):
+        print('Creating directory: {0}'.format(os.path.join(workdir,'stack')))
+        os.makedirs(os.path.join(workdir,'stack'))
+    else:
+        print('Directory {0} already exists.'.format(os.path.join(workdir,'stack')))
+
+    if inputFiles in ['interferogram', 'Interferogram', 'int']:
+        domainName = 'Interferogram'
+        intList = glob.glob(os.path.join(workdir,'unwrappedPhase','*.vrt'))
+        print('Number of interferograms discovered: ', len(intList))
+        Dlist = intList
+    elif inputFiles in ['coherence', 'Coherence', 'coh']:
+        domainName = 'Coherence'
+        cohList = glob.glob(os.path.join(workdir,'coherence','*.vrt'))
+        print('Number of coherence discovered: ', len(cohList))
+        Dlist = cohList
+    elif inputFiles in ['connectedComponents','connectedComponent','connComp']:
+        domainName = 'connectedComponents'
+        connCompList = glob.glob(os.path.join(workdir,'connectedComponents','*.vrt'))
+        print('Number of connectedComponents discovered: ', len(connCompList))
+        Dlist = connCompList
+    else:
+        print('Stacks can be created for interferogram, coherence and connectedComponent VRT files')
+
+    for ind, data in enumerate(Dlist):
+        width = None
+        height = None
+        path = None
+
+        ds = gdal.Open(data, gdal.GA_ReadOnly)
+        width = ds.RasterXSize
+        height = ds.RasterYSize
+
+        ds = None
+
+    # setting up a subset of the stack
+    ymin, ymax, xmin, xmax = [0 , height, 0 , width]
+    
+    xsize = xmax - xmin
+    ysize = ymax - ymin
+
+    with open( os.path.join(workdir,'stack', (outputFileName+'.vrt')), 'w') as fid:
+        fid.write( '<VRTDataset rasterXSize="{xsize}" rasterYSize="{ysize}">\n'.format(xsize=xsize, ysize=ysize))
+
+        for ind, data in enumerate(Dlist):
+            metadata = {}
+            dates = data.split('/')[2][:-4]
+            width = None
+            height = None
+            path = None
+
+            ds = gdal.Open(data, gdal.GA_ReadOnly)
+            width = ds.RasterXSize
+            height = ds.RasterYSize
+            ds = None
+
+
+            metadata['wavelength'] = wavelength
+            # metadata['ACQUISITION_TIME'] = os.path.basename(os.path.dirname(slc))
+            metadata['utcTime'] = UTC_time[dates]
+
+            path = os.path.abspath(data)
+
+            outstr = '''    <VRTRasterBand dataType="CFloat32" band="{index}">
+        <SimpleSource>
+            <SourceFilename>{path}</SourceFilename>
+            <SourceBand>1</SourceBand>
+            <SourceProperties RasterXSize="{width}" RasterYSize="{height}" DataType="CFloat32"/>
+            <SrcRect xOff="{xmin}" yOff="{ymin}" xSize="{xsize}" ySize="{ysize}"/>
+            <DstRect xOff="0" yOff="0" xSize="{xsize}" ySize="{ysize}"/>
+        </SimpleSource>
+        <Metadata domain='{domainName}'>
+            <MDI key="Dates">{dates}</MDI>
+            <MDI key="Wavelength (m)">{wvl}</MDI>
+            <MDI key="UTC Time">{acq}</MDI>
+        </Metadata>
+    </VRTRasterBand>\n'''.format(domainName=domainName,width=width, height=height,
+                                xmin=xmin, ymin=ymin,
+                                xsize=xsize, ysize=ysize,
+                                dates=dates, acq=metadata['utcTime'],
+                                wvl = metadata['wavelength'], index=ind+1,
+                                path = path)
+            fid.write(outstr)
+
+        fid.write('</VRTDataset>')
+        print(outputFileName, ' stack generated')
+
+################################################################################
+
 
 if __name__ == '__main__':
     '''
@@ -65,6 +180,9 @@ if __name__ == '__main__':
     print('\n'+'\n'+"########################################")
     print("fn 'merged_productbbox': extract/merge productBoundingBox layers for each pair and update dict, report common track bbox (default is to take common intersection, but user may specify union), and expected shape for DEM."+'\n')
     standardproduct_info.products[1], standardproduct_info.bbox_file, prods_TOTbbox, arrshape, proj = merged_productbbox(standardproduct_info.products[1], os.path.join(inps.workdir,'productBoundingBox'), standardproduct_info.bbox_file, inps.croptounion)
+
+    wavelength = standardproduct_info.products[0][0]['wavelength'][0].data
+
     # Load mask (if specified).
     if inps.mask is not None:
         inps.mask=gdal.Warp('', inps.mask, options=gdal.WarpOptions(format="MEM", cutlineDSName=prods_TOTbbox, outputBounds=open_shapefile(standardproduct_info.bbox_file, 0, 0).bounds, dstNodata=0))
@@ -84,5 +202,16 @@ if __name__ == '__main__':
 
 
     # Only extract layers needed for TS analysis
-    layers=['unwrappedPhase','coherence','bPerpendicular','incidenceAngle','lookAngle']
+    layers=['unwrappedPhase','coherence','bPerpendicular']
+    print('Extracting unwrapped phase, coherence, perpendicular baseline and connected components layers of all products')
     export_products(standardproduct_info.products[1], standardproduct_info.bbox_file, prods_TOTbbox, layers, dem=demfile, lat=Latitude, lon=Longitude, mask=inps.mask, outDir=inps.workdir)
+
+    layers=['incidenceAngle','lookAngle']
+    print('Extracting incidence angle and look angle of the first product')
+    export_products([standardproduct_info.products[1][0]], standardproduct_info.bbox_file, prods_TOTbbox, layers, dem=demfile, lat=Latitude, lon=Longitude, mask=inps.mask, outDir=inps.workdir) ##Only the first product is written
+
+
+    if inps.stack==True:
+        generateStack('interferogram','interfStack')
+        generateStack('coherence','cohStack')
+        generateStack('connectedComponents','connCompStack')
