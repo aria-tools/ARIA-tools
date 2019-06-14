b607af24ca32e5189cb70b12ce813d12131ba8d9
Brett Buzzanga <bbuzz318@icloud.com>
2019-06-14 11:19:08 -0700

    Restructing of API internals to parse JSON instead of overwriting .py

--- tools/python/productAPI.py
+++ tools/python/productAPI.py
@@ -2,7 +2,7 @@
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-# Author: Brett Buzzanga
+# Author: Brett A. Buzzanga
 # Copyright 2019, by the California Institute of Technology. ALL RIGHTS
 # RESERVED. United States Government Sponsorship acknowledged.
 #
@@ -12,6 +12,7 @@ import os
 import os.path as op
 import math
 import re
+import json
 import requests
 import argparse
 import importlib
@@ -21,9 +22,9 @@ from datetime import datetime, timedelta
 def createParser():
     """ Download a bulk download script and execute it """
     parser = argparse.ArgumentParser(description='Program to download all GUNW products for a single track.\nDetails on parameters at: https://www.asf.alaska.edu/get-data/learn-by-doing/',
-                                     epilog='Examples of use:\n\t productAPI.py --track 004 --output count\n\t productAPI.py --bbox "36.75 37.225 -76.655 -75.928"\n\t productAPI.py -t 004 --start "1 year ago" --now now',
+                                     epilog='Examples of use:\n\t productAPI.py --track 004 --output count\n\t productAPI.py --bbox "36.75 37.225 -76.655 -75.928"\n\t productAPI.py -t 004 --start "1 year ago" --end now',
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-o', '--output', dest='output', default='Download', type=str, help='Output type, default is "Download". Only "Download" and "Count" are currently supported.')
+    parser.add_argument('-o', '--output', dest='output', default='Download', type=str, help='Output type, default is "Download". "Download", "Count", and "Kml" are currently supported.')
     parser.add_argument('-t', '--track', dest='track', default=None, type=str, help='track to download')
     parser.add_argument('-b', '--bbox', dest='bbox',  default=None, type=str, help='Lat/Lon Bounding SNWE')
     parser.add_argument('-w', '--workdir', dest='wd', default='./products', type=str, help='Specify directory to deposit all outputs. Default is "products" in local directory where script is launched.')
@@ -32,6 +33,7 @@ def createParser():
     parser.add_argument('-g', '--daysgt', dest='daysgt', default=None, type=int, help='Take pairs with a temporal baseline -- days greater than this value. Example, annual pairs: productAPI.py -t 004 --daysgt 364.')
     parser.add_argument('-l', '--dayslt', dest='dayslt', default=None, type=int, help='Take pairs with a temporal baseline -- days less than this value.')
     parser.add_argument('-d', '--direction', dest='flightdir', default=None, type=str, help='Flight direction, options: ascending, a, descending, d')
+    parser.add_argument('-v', '--verbose', dest='v', default=True, type=bool, help='Print products to be downloaded to stdout')
     return parser
 
 def cmdLineParse(iargs=None):
@@ -57,7 +59,6 @@ def cmdLineParse(iargs=None):
             raise Exception('Date format not understood; see https://www.asf.alaska.edu/get-data/learn-by-doing/ for options')
 
     inps.start = i1[0]; inps.end = i1[1]
-
     return inps
 
 class Downloader(object):
@@ -65,29 +66,32 @@ class Downloader(object):
         self.inps        = inps
         self.inps.output = self.inps.output.title()
         self.inps.wd     = op.abspath(self.inps.wd)
-
+        self.url_base    = 'https://api.daac.asf.alaska.edu/services/search/param?'
         if not op.exists(self.inps.wd): os.mkdir(self.inps.wd)
-        os.sys.path.append(self.inps.wd);
 
     def __call__(self):
-        url         = self.form_url()
-        path_script = self.write_dl_script(url)
-        module = importlib.import_module(op.splitext(op.basename(path_script))[0])
+        url       = self.form_url()
+        dict_prod = self.parse_json(url)
 
-        # setup and run the newly created download script
-        os.chdir(self.inps.wd)
-        signal.signal(signal.SIGINT, self.signal_handler)
-        os.sys.argv = []
-        try:
-            downloader = module.bulk_downloader()
-        except:
-            raise Exception('There was a problem with \n\t{};\n\t Recommend to open and find it'.format(path_script))
-        downloader.download_files()
-        downloader.print_summary()
+        script = requests.post('{}&output={}'.format(self.url_base,
+                                        self.inps.output), data=dict_prod).text
+        if self.inps.output == 'Count':
+            print ('\nFound -- {} -- products'.format(script.strip()))
+
+        elif self.inps.output == 'Kml':
+            dst = self._fmt_dst()
+            print (script, file=open(dst, 'w'))
+            print ('Wrote .KMZ to:\n\t {}'.format(dst))
+
+        elif self.inps.output == 'Download':
+            os.chdir(self.inps.wd)
+            os.sys.argv = []
+            exec(script, globals())
+        return
+
 
     def form_url(self):
-        url_base  = 'https://api.daac.asf.alaska.edu/services/search/param?asfplatform=Sentinel-1%20Interferogram%20(BETA)'
-        url = '{}&output={}'.format(url_base, self.inps.output)
+        url = '{}asfplatform=Sentinel-1%20Interferogram%20(BETA)&output=JSON'.format(self.url_base)
         if self.inps.track:
             url += '&relativeOrbit={}'.format(self.inps.track)
         if self.inps.bbox:
@@ -103,66 +107,27 @@ class Downloader(object):
         print (url)
         return url
 
-    def write_dl_script(self, url):
-        """Remove non gunw products from a download script """
-        script = requests.get(url).text.split('\n')
-        dst    = self._fmt_dst()
-        if self.inps.output == 'Count':
-            print ('\nFound -- {} -- products, INCLUDING the on-demand layers'.format(script[0]))
-            os.sys.exit(0)
+    def parse_json(self, url):
+        j        = json.loads(requests.get(url).text)[0]
+        if len(j) == 0:
+            raise Exception('No products found with given url')
 
+        prod_ids = []
+        for prod in j:
+            id = prod['product_file_id']
+            if 'layer' in prod['downloadUrl']: continue
+            elif (self.inps.daysgt or self.inps.dayslt):
+                match = re.search(r'\d{8}_\d{8}', id).group()
+                end, st = [datetime.strptime(i, '%Y%m%d').date() for i in match.split('_')]
+                elap = (end - st).days
+                if self.inps.daysgt and not (elap > self.inps.daysgt): continue
+                if self.inps.dayslt and not (elap < self.inps.dayslt): continue
 
-        elif self.inps.output == 'Kml':
-            print ('\n'.join(script), file=open(dst, 'w'))
-            print ('Wrote .KMZ to:\n\t {}'.format(dst))
-            os.sys.exit(0)
+            prod_ids.append(id)
+            if self.inps.v: print ('Will process: {}'.format(id))
 
-        elif self.inps.output == 'Kml':
-            print ('\n'.join(script), file=open(dst, 'w'))
-            print ('Wrote .KMZ to:\n\t {}'.format(dst))
-            quit()
-
-        elif self.inps.output == 'Download':
-            script_gunw = []
-            for i, line in enumerate(script):
-                ## skip on demand layers
-                if 'reformat' in line:
-                    if not script[i+1]:
-                        script_gunw.append(']')
-                ## subset by temporal baseline
-                elif 'GUNW' in line and (self.inps.daysgt or self.inps.dayslt):
-                    match = re.search(r'\d{8}_\d{8}', line).group()
-                    end, st = [datetime.strptime(i, '%Y%m%d').date() for i in match.split('_')]
-                    elap = (end - st).days
-                    if self.inps.daysgt and not (elap > self.inps.daysgt):
-                        if '[' in line: script_gunw.append('        self.files = [');
-                        if ']' in line: script_gunw.append('        ]');
-                        continue
-                    if self.inps.dayslt and not (elap < self.inps.dayslt):
-                        if '[' in line: script_gunw.append('        self.files = [');
-                        if ']' in line: script_gunw.append('        ]');
-                        continue
-                    script_gunw.append(line)
-                    print ('Will process: {}'.format(match))
-                else:
-                    script_gunw.append(line)
-
-            script_exec = '\n'.join(script_gunw)
-            print (script_exec, file=open(dst, 'w'))
-
-
-        else:
-            raise Exception('{} output type not currently supported'.format(self.inps.output))
-        return dst
-
-    # from bulk download script
-    def signal_handler(sig, frame):
-        """ A routine that handles trapped signals """
-        global abort
-        sys.stderr.output("\n > Caught Signal. Exiting!\n")
-        abort = True # necessary to cause the program to stop
-        raise SystemExit  # this will only abort the thread that the ctrl+c was caught in
-        return
+        data = {'product_list': ','.join(prod_ids)}
+        return data
 
     ## utility functions
     def _get_bbox(self):
@@ -189,14 +154,11 @@ class Downloader(object):
                     WSEN_fmt.append(math.floor(float(coord)))
                 else:
                     WSEN_fmt.append(math.ceil(float(coord)))
-            dst = '{}_{}W{}S{}E{}N'.format(dst_base, *WSEN_fmt)
+            dst = '{}_{}W{}S{}E{}N.kmz'.format(dst_base, *WSEN_fmt)
 
-        if self.inps.output == 'Kml':
-            dst += '.kmz'
-        elif self.inps.output == 'Download':
-            dst += '.py'
+        dst += '.kmz'
         return dst
-      
+
 if __name__ == '__main__':
     inps = cmdLineParse()
 
