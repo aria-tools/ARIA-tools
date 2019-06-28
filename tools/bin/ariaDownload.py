a64d8e3c74d9e044e844137ddbf6dc99f5b1b8cf
Brett Buzzanga <bbuzz318@icloud.com>
2019-06-28 09:59:08 -0700

    Date order independent; kmz option supported

--- tools/bin/ariaDownload.py
+++ tools/bin/ariaDownload.py
@@ -21,18 +21,18 @@ from datetime import datetime, timedelta
 
 def createParser():
     """ Download a bulk download script and execute it """
-    parser = argparse.ArgumentParser(description='Program to download all GUNW products for a single track.',
-                                     epilog='Examples of use:\n\t productAPI.py --track 004 --output count\n\t productAPI.py --bbox "36.75 37.225 -76.655 -75.928"\n\t productAPI.py -t 004 --start 20190101' ,
+    parser = argparse.ArgumentParser(description='Command line interface to download GUNW products from the ASF DAAC. GUNW products are hosted at the NASA ASF DAAC.\nDownloading them requires a NASA Earthdata URS user login and requires users to add “ARIA Product Search” to their URS approved applications.',
+                                     epilog='Examples of use:\n\t ariaDownload.py --track 004 --output count\n\t ariaDownload.py --bbox "36.75 37.225 -76.655 -75.928"\n\t ariaDownload.py -t 004 --start 20190101' ,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-o', '--output', dest='output', default='Download', type=str, help='Output type, default is "Download". "Download", "Count", and "Kml" are currently supported.')
+    parser.add_argument('-o', '--output', dest='output', default='Download', type=str, help='Output type, default is "Download". "Download", "Count", and "Kmz" are currently supported.')
     parser.add_argument('-t', '--track', dest='track', default=None, type=str, help='track to download')
-    parser.add_argument('-b', '--bbox', dest='bbox',  default=None, type=str, help='Lat/Lon Bounding SNWE')
+    parser.add_argument('-b', '--bbox', dest='bbox',  default=None, type=str, help='Lat/Lon Bounding SNWE, or GDAL-readable file containing POLYGON geometry.')
     parser.add_argument('-w', '--workdir', dest='wd', default='./products', type=str, help='Specify directory to deposit all outputs. Default is "products" in local directory where script is launched.')
-    parser.add_argument('-s', '--start', dest='start', default=None, type=str, help='Start date as YYYYMMDD; If none provided, starts at beginning of Sentinel record (2015).')
+    parser.add_argument('-s', '--start', dest='start', default=None, type=str, help='Start date as YYYYMMDD; If none provided, starts at beginning of Sentinel record (2014).')
     parser.add_argument('-e', '--end', dest='end', default=None, type=str, help='End date as YYYYMMDD. If none provided, ends today.')
     parser.add_argument('-l', '--daysless', dest='dayslt', default=None, type=int, help='Take pairs with a temporal baseline -- days less than this value.')
-    parser.add_argument('-m', '--daysmore', dest='daysgt', default=None, type=int, help='Take pairs with a temporal baseline -- days greater than this value. Example, annual pairs: productAPI.py -t 004 --daysmore 364.')
-    parser.add_argument('-i', '--ifg', dest='ifg', default=None, type=str, help='Retrieve one interferogram by its start/end date, specified as YYYYMMDD_YYYYMMDD.')
+    parser.add_argument('-m', '--daysmore', dest='daysgt', default=None, type=int, help='Take pairs with a temporal baseline -- days greater than this value. Example, annual pairs: ariaDownload.py -t 004 --daysmore 364.')
+    parser.add_argument('-i', '--ifg', dest='ifg', default=None, type=str, help='Retrieve one interferogram by its start/end date, specified as YYYYMMDD_YYYYMMDD (order independent)')
     parser.add_argument('-d', '--direction', dest='flightdir', default=None, type=str, help='Flight direction, options: ascending, a, descending, d')
     parser.add_argument('-v', '--verbose', dest='v', action='store_true', help='Print products to be downloaded to stdout')
     return parser
@@ -48,6 +48,11 @@ def cmdLineParse(iargs=None):
     if not inps.track and not inps.bbox:
         raise Exception('Must specify either a bbox or track')
 
+    if not inps.output.lower() in ['count', 'kml', 'kmz', 'download']:
+        raise Exception ('Incorrect output keyword. Choose "count", "kmz", or "download"')
+
+    if inps.output.lower() == 'kmz': inps.output = 'Kml'
+
     return inps
 
 class Downloader(object):
@@ -60,7 +65,6 @@ class Downloader(object):
     def __call__(self):
         url       = self.form_url()
         dict_prod = self.parse_json(url)
-
         script = requests.post('{}&output={}'.format(self.url_base,
                                         self.inps.output), data=dict_prod).text
         if self.inps.output == 'Count':
@@ -95,17 +99,21 @@ class Downloader(object):
     def parse_json(self, url):
         j        = json.loads(requests.get(url).text)[0]
         if len(j) == 0:
-            raise Exception('No products found with given url')
+            raise Exception('No products found with given url; check inputs for errors.')
 
         prod_ids = []
+        i=0
         for prod in j:
             if 'layer' in prod['downloadUrl']: continue
-
+            i+=1
             id = prod['product_file_id']
             match = re.search(r'\d{8}_\d{8}', id).group()
-            end, st = [datetime.strptime(i, '%Y%m%d').date() for i in match.split('_')]
+            dates = [datetime.strptime(i, '%Y%m%d').date() for i in match.split('_')]
+            st, end = sorted(dates)
+
             if self.inps.ifg:
-                st1, end1 = [datetime.strptime(i, '%Y%m%d').date() for i in self.inps.ifg.split('_')]
+                dates1 = [datetime.strptime(i, '%Y%m%d').date() for i in self.inps.ifg.split('_')]
+                st1, end1 = sorted(dates1)
                 if st1 != st or end1 != end: continue
 
             if (self.inps.start or self.inps.end):
@@ -119,6 +127,9 @@ class Downloader(object):
 
             prod_ids.append(id)
             if self.inps.v: print ('Found: {}'.format(id))
+
+        if len(prod_ids) == 0:
+            raise Exception('No products found that satisfy requested conditions.')
 
         data = {'product_list': ','.join(prod_ids)}
         return data
