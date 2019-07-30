58c7030f572251f382092d7f7ac79cadc8206e7a
ehavazli <emre.havazli@jpl.nasa.gov>
2019-07-30 09:20:22 -0700

    New tool: - create bounding box files to use with ARIA-tools from Google Earth polygon kml files

---
+++ tools/bin/ariaKml2box.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Author: Simran Sangha, David Bekaert, & Emre Havazli
+# Copyright 2019, by the California Institute of Technology. ALL RIGHTS
+# RESERVED. United States Government Sponsorship acknowledged.
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Import functions
+from ARIAtools.kml2box import cmdLineParse,main
+
+if __name__ == '__main__':
+    '''
+        Main driver.
+    '''
+    inps = cmdLineParse()
+main(inps)
