b9191a01cad244c8507574ff640e1957e63bc0ef
Simran S Sangha <ssangha@sar.jpl.nasa.gov>
2019-09-12 10:35:32 -0700

    Added KML generation for each track & date

--- tools/ARIAtools/AOIdef_assist.py
+++ tools/ARIAtools/AOIdef_assist.py
@@ -1,32 +1,21 @@
 #!/usr/bin/env python3
 
-import os, sys, glob
+import os
 import argparse
-import configparser
-import  datetime
+import datetime
+from datetime import datetime, timedelta
 import numpy as np
 import csv
-import re
-import matplotlib.dates as mdates
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
+import itertools
+import pandas as pd
+from collections import OrderedDict
+from osgeo import ogr
 
 import matplotlib as mpl
-from matplotlib.ticker import AutoMinorLocator
-label_size = 50
-#label_font = 'Arial'
-#!#print(mpl.rcParams.keys())
-mpl.rcParams['xtick.labelsize'] = 20
-mpl.rcParams['ytick.labelsize'] = 20
-mpl.rcParams['axes.labelsize'] = label_size
-mpl.rcParams['axes.titlesize'] = label_size
-#mpl.rcParams['font.family'] = label_font
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
 
-import itertools
-
-import pandas as pd
-
-from datetime import datetime, timedelta
+from shapely.geometry import Polygon, MultiPolygon
 
 def createParser():
     parser = argparse.ArgumentParser( description='Preparing preliminary plot of frame extents')
@@ -57,7 +46,6 @@ def main(iargs=None):
         os.mkdir(inps.workdir)
 
     inps.lat_bounds=[float(val) for val in inps.lat_bounds.split()]
-######Sensing lats
     csvfile_name=os.path.abspath(inps.imgfile)
     with open(csvfile_name, newline='') as csvfile:
         frame_metadata = csv.reader(csvfile)
@@ -88,11 +76,11 @@ def main(iargs=None):
                 OG_frames.append(i[0])
                 OG_metaindices.append(i[5]+i[6]+i[7])
                 if i[15]>i[21] and i[15]<i[17]: #descending
-                    OG_tot_str_all_burst_lats.append([i[15:23]])
+                    OG_tot_str_all_burst_lats.append(i[15:23])
                 elif i[15]>i[21] and i[15]>i[17]: #ascending glitch
-                    OG_tot_str_all_burst_lats.append([[i[15],i[16],i[19],i[20],i[17],i[18],i[21],i[22]]])
+                    OG_tot_str_all_burst_lats.append([i[15],i[16],i[19],i[20],i[17],i[18],i[21],i[22]])
                 else: #ascending glitch
-                    OG_tot_str_all_burst_lats.append([[i[19],i[20],i[21],i[22],i[15],i[16],i[17],i[18]]])
+                    OG_tot_str_all_burst_lats.append([i[19],i[20],i[21],i[22],i[15],i[16],i[17],i[18]])
                 OG_all_scene_frame.append(i[6])
                 OG_all_pass.append(i[24][0])
 
@@ -106,11 +94,11 @@ def main(iargs=None):
                 OG_frames.append(i[0])
                 OG_metaindices.append("RAW")
                 if i[15]>i[21] and i[15]<i[17]: #descending
-                    OG_tot_str_all_burst_lats.append([i[15:23]])
+                    OG_tot_str_all_burst_lats.append(i[15:23])
                 elif i[15]>i[21] and i[15]>i[17]: #ascending glitch
-                    OG_tot_str_all_burst_lats.append([[i[15],i[16],i[19],i[20],i[17],i[18],i[21],i[22]]])
+                    OG_tot_str_all_burst_lats.append([i[15],i[16],i[19],i[20],i[17],i[18],i[21],i[22]])
                 else: #ascending glitch
-                    OG_tot_str_all_burst_lats.append([[i[19],i[20],i[21],i[22],i[15],i[16],i[17],i[18]]])
+                    OG_tot_str_all_burst_lats.append([i[19],i[20],i[21],i[22],i[15],i[16],i[17],i[18]])
                 OG_all_scene_frame.append(i[6])
                 OG_all_pass.append(i[24][0])
 
@@ -179,10 +167,6 @@ def main(iargs=None):
         unique_array_all_scene_lat_dates=range(len(unique_all_scene_lat_dates))
         pd_dates = pd.to_datetime(dateList)
         pd_dates = [datetime(i.year, i.month, i.day) for i in pd_dates]
-        #print("pd_dates")
-        #print(pd_dates)
-        #print(len(pd_dates))
-        #print(len(unique_all_scene_lat_dates))
         ax.set_xticks(pd_dates)
 
         mostcommonvalue=max(zip((all_scene_lat_dates.count(item) for item in set(all_scene_lat_dates)), set(all_scene_lat_dates)))
@@ -192,19 +176,39 @@ def main(iargs=None):
         stack_master_overlap = list(itertools.chain.from_iterable(stack_master_overlap))
         stack_master_overlap=max(stack_master_overlap)- min(stack_master_overlap)
 
+        #open KML
+        ds = ogr.GetDriverByName('LIBKML').CreateDataSource(os.path.join(inps.workdir,'track%s%s_frames.kml'%(all_pass[0],all_scene_frame[0])))
+
         for i in array_all_scene_lat_dates:
             date_ind=[l for l, e in enumerate(all_scene_lat_dates) if e==all_scene_lat_dates[i]]
+
+            #only initiate KML layer if iterating through first instance of date in list
+            if i == date_ind[0]:
+                # create KML layer
+                layer = ds.CreateLayer(str(all_scene_lat_dates[i]), None, ogr.wkbPolygon)
+                layer.CreateField(ogr.FieldDefn('id', ogr.OFTInteger)) #Add 1 attribute
+
             lat_ind=[str_all_burst_lats[i] for i in date_ind]
             lat_ind.sort()
             lat_ind=[str(abs(float(x)-float(lat_ind[i-1]))) for i, x in enumerate(lat_ind)][1:]
             scene_overlap=[float(str_all_burst_lats[i]) for i in date_ind]
             scene_overlap=max(scene_overlap)-min(scene_overlap)
             if all_scene_lat_dates[i]==mostcommonvalue[1] and all_metaindices[i]!='RAW':
-                mostcommonvalue_array.append(tot_str_all_burst_lats[i][0])
+                mostcommonvalue_array.append(tot_str_all_burst_lats[i])
             if ((scene_overlap/stack_master_overlap)*100)>inps.min_frames and all(float(i)<1.75 for i in lat_ind)==True and all_metaindices[i]!='RAW':
-                ax.plot(pd_dates[unique_all_scene_lat_dates.index(all_scene_lat_dates[i])],float(str_all_burst_lats[i]),'ko',markersize=10)
+                ax.plot(pd_dates[unique_all_scene_lat_dates.index(all_scene_lat_dates[i])],float(str_all_burst_lats[i]),'ko',markersize=10, label="valid frame")
+                ###only write to KML layer if valid frame
+                # Create a new feature (attribute and geometry)
+                feat = ogr.Feature(layer.GetLayerDefn())
+                feat.SetField('id', i)
+                # Make a geometry, from input Shapely object
+                bbox = Polygon(np.column_stack((np.array([tot_str_all_burst_lats[i][1],tot_str_all_burst_lats[i][3],tot_str_all_burst_lats[i][7],tot_str_all_burst_lats[i][5],tot_str_all_burst_lats[i][1]]),np.array([tot_str_all_burst_lats[i][0],tot_str_all_burst_lats[i][2],tot_str_all_burst_lats[i][6],tot_str_all_burst_lats[i][4],tot_str_all_burst_lats[i][0]])))) #Pass lons/lats to create polygon
+                geom = ogr.CreateGeometryFromWkb(bbox.wkb)
+                feat.SetGeometry(geom)
+                layer.CreateFeature(feat)
+
             if ((scene_overlap/stack_master_overlap)*100)<=inps.min_frames and all_metaindices[i]!='RAW':
-                ax.plot(pd_dates[unique_all_scene_lat_dates.index(all_scene_lat_dates[i])],float(str_all_burst_lats[i]),'ro',markersize=20)
+                ax.plot(pd_dates[unique_all_scene_lat_dates.index(all_scene_lat_dates[i])],float(str_all_burst_lats[i]),'ro',markersize=20, label="invalid frame")
                 ax.get_xticklabels()[unique_all_scene_lat_dates.index(all_scene_lat_dates[i])].set_color("red")
                 if all_scene_lat_dates[i] in dateList:
                     dateList.remove(all_scene_lat_dates[i])
@@ -215,13 +219,20 @@ def main(iargs=None):
                 if all_scene_lat_dates[i] in dateList:
                     dateList.remove(all_scene_lat_dates[i])
             if all_metaindices[i]=='RAW':
-                ax.plot(pd_dates[unique_all_scene_lat_dates.index(all_scene_lat_dates[i])],float(str_all_burst_lats[i]),'gx',markersize=25)
+                ax.plot(pd_dates[unique_all_scene_lat_dates.index(all_scene_lat_dates[i])],float(str_all_burst_lats[i]),'gx',markersize=25, label="only raw available")
                 #track unprocessed raw frames
                 fi = open(os.path.join(inps.workdir,'%s%s_rawframes_to_download.txt'%(all_pass[0],all_scene_frame[0])),'a')
                 fi.write(all_frames[i])
                 fi.write('\n')
+
+            #only close KML layer if iterating through late instance of date in list
+            if i == date_ind[-1]:
+                # close KML layer
+                layer = feat = geom = None
         mostcommonvalue_array.sort()
         mostcommonvalue_array.insert(0,[mostcommonvalue[1]])
+        # close KML file
+        ds = layer = feat = geom = None
 
         ###GMT script
         f = open(os.path.join(inps.workdir,'gmtbounds.txt'),'a')
@@ -320,17 +331,22 @@ def main(iargs=None):
             f.write(i)
             f.write('\n')
 
+        # add legend
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = OrderedDict(zip(labels, handles))
+        plt.legend(by_label.values(), by_label.keys(), bbox_to_anchor=(1.005, 1), loc='upper left', borderaxespad=0.)
 
-        ###Make lat extents plot for each track
-        ax.set_xlabel('Date')
-        ax.set_ylabel('Lat')
-        ax.set_title('%s%s lat extents'%(all_pass[0],all_scene_frame[0]))
-        # beautify the x-labels
-        #plt.gcf().autofmt_xdate()
+        # make lat extents plot for each track
+        ax.set_xlabel('Date',weight='bold')
+        ax.set_ylabel('Lat',weight='bold')
+        ax.set_title('%s%s lat extents'%(all_pass[0],all_scene_frame[0]),weight='bold')
+        ax.margins(x=0)
         myFmt = mdates.DateFormatter('%Y%m%d')
         plt.gca().xaxis.set_major_formatter(myFmt)
         plt.xticks(rotation=90)
         plt.tight_layout()
+
+        # saving the figure
         plt.savefig(os.path.join(inps.workdir,'%s%s_lat_extents.eps'%(all_pass[0],all_scene_frame[0])))
         plt.close()
 
