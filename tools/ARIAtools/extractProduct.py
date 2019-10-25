08ebec59dbc0ac509bd93a4a407f356cc7b07221
sssangha <sssangha@ucla.edu>
2019-10-25 11:26:41 -0700

    Remove print statement used for debugging

--- tools/ARIAtools/extractProduct.py
+++ tools/ARIAtools/extractProduct.py
@@ -440,7 +440,6 @@ def export_products(full_product_dict, bbox_file, prods_TOTbbox, layers, dem=Non
             #If necessary, resample raster
             if multilooking is not None and key!='unwrappedPhase' and key!='connectedComponents':
                 resampleRaster(outname, multilooking, bounds, prods_TOTbbox, outputFormat=outputFormat, num_threads=num_threads)
-                print("outname",outname)
 
         prog_bar.close()
     return
