112f0002e7f34067de01bbfb37ecdbc9f330ec07
sssangha <sssangha@ucla.edu>
2020-04-25 18:46:51 -0700

    Passed time with decimal seconds

--- tools/ARIAtools/tsSetup.py
+++ tools/ARIAtools/tsSetup.py
@@ -85,7 +85,7 @@ def extractUTCtime(aria_prod):
         #Write calculated UTC time into a dictionary with associated pair names as keys
         timeDelta = (maxMidTime - minMidTime)/2
         utcTime = (minMidTime+timeDelta).time()
-        utcDict[pairName[0]] = utcTime
+        utcDict[pairName[0]] = utcTime.strftime("%H:%M:%S.%f")
     return utcDict
 
 def generateStack(aria_prod,inputFiles,outputFileName,workdir='./'):
