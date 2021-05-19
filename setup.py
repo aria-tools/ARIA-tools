8af8da404243d23b0ee451bfbba8149ecc04e3fd
bbuzz31 <bbuzz318@icloud.com>
2021-05-19 09:29:11 -0700

    Use absolute paths in setup.py

--- setup.py
+++ setup.py
@@ -13,22 +13,33 @@ import os
 
 # Path where relax third party is download to
 # Note Min-Cost-Flow-Class has its own liscence agreement, users to ensure proper use of third party license agreements.
-relaxPath='tools/thirdParty/Min-Cost-Flow-Class'
+root_path  = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tools')
+bind_path  = os.path.join(root_path, 'bindings')
+bind_dirs  = [os.path.join(bind_path, rdir) for \
+                        rdir in 'relaxIVdriver.cpp unwcompmodule.cpp'.split()]
+
+relax_path = os.path.join(root_path, 'thirdParty', 'Min-Cost-Flow-Class')
+relax_dirs = [os.path.join(relax_path, rdir) for \
+                        rdir in 'MCFClass OPTUtils RelaxIV'.split()]
+
+aria_scripts = [os.path.join(root_path, 'bin', script) for \
+                script in 'ariaPlot.py ariaDownload.py ariaExtract.py '\
+                   'ariaTSsetup.py ariaAOIassist.py ariaMisclosure.py'.split()]
 
 # If third party package is found compile as well
-if os.path.isdir(relaxPath):
+if os.path.isdir(relax_path):
     print('Installing ARIA-tools with support for RelaxIV')
-    module1 = Extension('ARIAtools.demo', sources = ['tools/bindings/relaxIVdriver.cpp',
-                                                     'tools/bindings/unwcompmodule.cpp',
-                                                     os.path.join(relaxPath,'RelaxIV/RelaxIV.C')], include_dirs=['tools/include' ,os.path.join(relaxPath,'MCFClass'),os.path.join(relaxPath,'OPTUtils'),os.path.join(relaxPath,'RelaxIV')])
+    module1 = Extension('ARIAtools.demo', sources=bind_dirs + 
+                     [os.path.join(relax_path, 'RelaxIV', 'RelaxIV.C')], 
+             include_dirs=[os.path.join(root_path, 'include')] + relax_dirs)
 
     setup (name = 'ARIAtools',
            version = '1.0',
            description = 'This is the ARIA tools package with RelaxIV support',
            ext_modules = [module1],
            packages=['ARIAtools'],
-           package_dir={'ARIAtools': 'tools/ARIAtools'},
-           scripts=['tools/bin/ariaPlot.py','tools/bin/ariaDownload.py','tools/bin/ariaExtract.py','tools/bin/ariaTSsetup.py','tools/bin/ariaAOIassist.py','tools/bin/ariaMisclosure.py'])
+           package_dir={'ARIAtools': os.path.join(root_path, 'ARIAtools')},
+           scripts=aria_scripts)
 else:
     # Third party package RelaxIV not found
     print('Installing ARIA-tools without support for RelaxIV')
@@ -37,5 +48,5 @@ else:
            version = '1.0',
            description = 'This is the ARIA tools package without RelaxIV support',
            packages=['ARIAtools'],
-           package_dir={'ARIAtools': 'tools/ARIAtools'},
-           scripts=['tools/bin/ariaPlot.py','tools/bin/ariaDownload.py','tools/bin/ariaExtract.py','tools/bin/ariaTSsetup.py','tools/bin/ariaAOIassist.py','tools/bin/ariaMisclosure.py'])
+           package_dir={'ARIAtools': os.path.join(root_path, 'ARIAtools')},
+           scripts=aria_scripts)
