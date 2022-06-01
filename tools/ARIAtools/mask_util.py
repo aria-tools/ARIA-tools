30f5a1df86ca4d4af6d8dccaf32779a7a291b604
bbuzz31 <bbuzz318@icloud.com>
2022-05-31 18:07:47 -0700

    Flush mask for new gdal

--- tools/ARIAtools/mask_util.py
+++ tools/ARIAtools/mask_util.py
@@ -146,8 +146,7 @@ def prep_mask(product_dict, maskfilename, bbox_file, prods_TOTbbox, proj,
         del mask_file, amp_file
 
     # crop/expand mask to DEM size?
-    ## just double check you cant lfush it out/mas
-    mask = gdal.Warp('', f'{maskfilename}.vrt', format='MEM',
+    mask = gdal.Warp('', maskfilename, format='MEM',
                     cutlineDSName=prods_TOTbbox, outputBounds=bounds,
                     width=arrshape[1], height=arrshape[0], multithread=True,
                     options=[f'NUM_THREADS={num_threads}'])
@@ -281,24 +280,24 @@ class NLCDMasker(object):
         ## write mask to disk
         path_mask = op.join(self.path_aria, 'mask')
         os.mkdirs(path_mask) if not op.exists(path_mask) else ''
-        dst = op.join(path_mask, 'NLCD_crop.msk')
+        dst = op.join(path_mask, f'NLCD_crop.msk')
         ds  = gdal.Translate(dst, ds_mask, format=outputFormat, outputType=gdal.GDT_Byte)
 
-        gdal.BuildVRT(dst + '.vrt', ds)
+        ds1 = gdal.BuildVRT(dst+'.vrt', ds)
 
         ## save a view of the mask
         arr = ds.ReadAsArray()
         plt.imshow(arr, cmap=plt.cm.Greys_r, interpolation='nearest')
         plt.colorbar();
         plt.title(f'Resampled mask\nDims: {arr.shape}')
-        plt.savefig(op.join(path_mask, 'NLCD_crop.msk.png'))
+        plt.savefig(f'{op.splitext(dst)[0]}.png')
 
         if test: self.__test__(ds_mask)
-        for dss in [ds_crop, ds_resamp, ds_mask, ds]:
-            ds.FlushCache()
-            del dss
+
+        del ds, ds1, ds_crop, ds_resamp, ds_mask
 
         return dst
+
 
     def __test__(self, ds_maskre):
         ## apply mask to dummy data FOR TESTING
@@ -310,6 +309,7 @@ class NLCDMasker(object):
         arr = np.where(arr>9990, np.nan, arr)
         plt.imshow(arr, cmap='jet_r', interpolation='nearest'); plt.colorbar()
         plt.title('Mask applied to dummy data'); plt.show()
+
 
     def _dummy_data(self, ds2match):
         """ Create raster of dummy data using the dem (for sizing); For test """
@@ -323,6 +323,7 @@ class NLCDMasker(object):
         ds  = arr2ds(ds2match, arr)
         arr1 = ds.ReadAsArray()
         return ds
+
 
     def _apply_mask(self, ds_mask, ds_2mask):
         """ Apply mask to test viewing """
