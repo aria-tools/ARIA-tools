826be2bb44811d5840bb9f91a13db4bc008ba0eb
cmarshak <charles.marshak@gmail.com>
2023-03-03 15:14:39 -0800

    only 310 and remove tssetup

--- tests/test.sh
+++ tests/test.sh
@@ -10,8 +10,7 @@ cd test_tmp
 ariaDownload.py -t 124 -w products --ifg 20180408_20180502 -v -o count
 #product url
 ariaDownload.py -t 124 -w products --ifg 20180408_20180502 -v -o url
-#product download
-#ariaDownload.py -t 124 -w products --ifg 20180408_20180502 -v
+
 cd products
 wget --no-clobber --load-cookies /tmp/cookies.txt --save-cookies /tmp/cookies.txt --keep-session-cookies https://grfn.asf.alaska.edu/door/download/S1-GUNW-A-R-124-tops-20180502_20180408-043106-21658N_19755N-PP-0dd0-v2_0_1.nc
 wget --no-clobber --load-cookies /tmp/cookies.txt --save-cookies /tmp/cookies.txt --keep-session-cookies https://grfn.asf.alaska.edu/door/download/S1-GUNW-A-R-124-tops-20180502_20180408-043040-20161N_18088N-PP-6704-v2_0_1.nc
@@ -25,15 +24,9 @@ ariaExtract.py -f "products/*.nc" -l coherence
 ariaExtract.py -f "products/*.nc" -l unwrappedPhase
 
 #time-series prep
-ariaTSsetup.py -d download -m download -f "products/*.nc"
+# ariaTSsetup.py -d download -m download -f "products/*.nc"
 #run loading of aria products in mintpy
-prep_aria.py -s stack/ -d DEM/SRTM_3arcsec.dem -i incidenceAngle/20180502_20180408.vrt -a azimuthAngle/20180502_20180408.vrt
+# prep_aria.py -s stack/ -d DEM/SRTM_3arcsec.dem -i incidenceAngle/20180502_20180408.vrt -a azimuthAngle/20180502_20180408.vrt
 
 #download and test NLCD mask
-cd products
-conda deactivate
-conda activate ARIA-tools
-wget --no-clobber --load-cookies /tmp/cookies.txt --save-cookies /tmp/cookies.txt --keep-session-cookies https://grfn.asf.alaska.edu/door/download/S1-GUNW-A-R-004-tops-20190325_20190301-230628-37654N_35777N-PP-13e7-v2_0_2.nc
-wget --no-clobber --load-cookies /tmp/cookies.txt --save-cookies /tmp/cookies.txt --keep-session-cookies https://grfn.asf.alaska.edu/door/download/S1-GUNW-A-R-004-tops-20190325_20190301-230628-37654N_35777N-PP-13e7-v2_0_2.nc
-cd ../
-ariaExtract.py -f "products/*004-*.nc" -m 'NLCD'
+ariaExtract.py -f "products/*.nc" -m 'NLCD'
