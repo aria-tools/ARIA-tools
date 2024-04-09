d24d92e2b2b966582982acb0de78469717936bf6
Simran S Sangha <sssangha@ucla.edu>
2024-04-09 16:21:41 -0700

    Make updates to ariaTSsetup

--- tools/bin/ariaTSsetup.py
+++ tools/bin/ariaTSsetup.py
@@ -71,8 +71,10 @@ def create_parser():
         '-d', '--demfile', dest='demfile', type=str, default='download',
         help='DEM file. Default is to download new DEM.')
     parser.add_argument(
-        '-p', '--projection', dest='projection', default='WGS84', type=str,
-        help='projection for DEM. By default WGS84.')
+        '-p', '--projection', dest='projection', default='4326', type=str,
+        help='EPSG projection code for DEM. By default 4326. '
+             'Specify "native" to pass most common '
+             'projection from stack.')
     parser.add_argument(
         '-b', '--bbox', dest='bbox', type=str, default=None,
         help='Provide either valid shapefile or Lat/Lon Bounding SNWE. -- '
@@ -94,14 +96,6 @@ def create_parser():
         help='Specify number of threads for multiprocessing operation in gdal. '
              'By default "2". Can also specify "All" to use all available '
              'threads.')
-    parser.add_argument(
-        '-sm', '--stitchMethod', dest='stitchMethodType', type=str,
-        default='sequential',
-        help='Method applied to stitch the unwrapped data. Allowed methods '
-             'are: "overlap", "2stage", and "sequential". "overlap" - '
-             'product overlap is minimized, "2stage" - minimization is done '
-             'on connected components, "sequential" - sequential minimization '
-             'of all overlapping connected components. Default is "overlap".')
     parser.add_argument(
         '-of', '--outputFormat', dest='outputFormat', type=str, default='VRT',
         help='GDAL compatible output format (e.g., "ENVI", "GTiff"). By '
@@ -412,9 +406,10 @@ def main():
     # (if bbox specified)
     LOGGER.info('Building ARIA product instance')
     standardproduct_info = ARIAtools.product.Product(
-        args.imgfile, bbox=args.bbox, workdir=args.workdir,
-        num_threads=args.num_threads, url_version=args.version,
-        nc_version=args.nc_version, verbose=args.verbose)
+        args.imgfile, bbox=args.bbox, projection=args.projection,
+        workdir=args.workdir, num_threads=args.num_threads,
+        url_version=args.version, nc_version=args.nc_version,
+        verbose=args.verbose)
 
 
     # extract/merge productBoundingBox layers for each pair and update dict,
@@ -486,6 +481,7 @@ def main():
 
     # Extract
     export_dict = {
+        'proj': proj,
         'bbox_file': standardproduct_info.bbox_file,
         'prods_TOTbbox': prods_TOTbbox,
         'demfile': demfile,
@@ -496,7 +492,6 @@ def main():
         'maskfile': maskfilename,
         'outDir': args.workdir,
         'outputFormat': args.outputFormat,
-        'stitchMethodType': args.stitchMethodType,
         'verbose': args.verbose,
         'num_threads': args.num_threads,
         'multilooking': args.multilooking
