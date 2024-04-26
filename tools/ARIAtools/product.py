ff3f9b7dc0eac1b3edd76559ff0d6b129fa60b67
Simran S Sangha <ssangha@trappist.jpl.nasa.gov>
2024-04-26 15:12:39 -0700

    Get rid of vestigial flag

--- tools/ARIAtools/product.py
+++ tools/ARIAtools/product.py
@@ -256,9 +256,6 @@ class Product:
         # enforced netcdf version
         self.nc_version = nc_version
 
-        # track if NISAR GUNW or not
-        self.is_nisar_file = False
-
         # enforced projection for output rasters
         self.projection = projection
 
@@ -436,7 +433,6 @@ class Product:
         # version accessed differently between URL vs downloaded product
         # vs NISAR and S1 GUNWs
         if basename.split('_')[0] == 'NISAR':
-            self.is_nisar_file = True
             version = basename.split('_')[-1][:-3]
             version = '.'.join(version)
             nc_version_check = [version]
