87df8141934574dc8c960067500154ff6da48cdd
Simran S Sangha <ssangha@trappist.jpl.nasa.gov>
2024-04-29 18:19:29 -0700

    Split long if statement conditional into multiple lines

--- tools/ARIAtools/extractProduct.py
+++ tools/ARIAtools/extractProduct.py
@@ -216,7 +216,7 @@ class MetadataQualityCheck:
                 # Unique bug-fix for bPerp layers with sign-flips
                 if ((self.prod_key == 'bPerpendicular' and
                     min(rsquaredarr) < 0.8 and max(std_errarr) > 0.1) and
-                     (negs_percent != 100 or negs_percent != 0)):
+                        (negs_percent != 100 or negs_percent != 0)):
 
                     # Circumvent Bperp sign-flip bug by comparing percentage of
                     # positive and negative values
@@ -944,9 +944,10 @@ def export_product_worker(
     outname = os.path.abspath(os.path.join(workdir, ifg_tag))
 
     # Extract/crop metadata layers
-    if (any(':/science/grids/imagingGeometry' in s for s in product) or
+    if (
+        any(':/science/grids/imagingGeometry' in s for s in product) or
         any(':/science/LSAR/GUNW/metadata/radarGrid/' in s for s in product)
-            ):
+    ):
         # make VRT pointing to metadata layers in standard product
         hgt_field, model_name, outname = prep_metadatalayers(
             outname, product, dem_expanded, layer, layers,
