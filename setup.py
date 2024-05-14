b5f8d065fb89807f746a095040586c7663414c3f
Simran S Sangha <sssangha@ucla.edu>
2024-05-14 11:38:26 -0700

    Remove relax from build

--- setup.py
+++ setup.py
@@ -12,12 +12,6 @@ import os
 import subprocess
 from datetime import datetime
 
-
-# Path where relax third party is download to
-# Note Min-Cost-Flow-Class has its own liscence agreement, users to ensure
-# proper use of third party license agreements.
-relaxPath = 'tools/thirdParty/Min-Cost-Flow-Class'
-
 # setup version; adapted from MintPy
 cmd = "git describe --tags"
 try:
@@ -45,41 +39,14 @@ except BaseException:
     date = datetime.strftime(datetime.today(), '%Y-%m-%d')
 
 print(f'ARIA-tools {version} {date}')
+print('Installing ARIA-tools')
 
-# If third party package is found compile as well
-if os.path.isdir(relaxPath):
-    print('Installing ARIA-tools with support for RelaxIV')
-    module1 = Extension(
-        'ARIAtools.demo',
-        sources=[
-            'tools/bindings/relaxIVdriver.cpp',
-            'tools/bindings/unwcompmodule.cpp',
-            os.path.join(relaxPath, 'RelaxIV/RelaxIV.C')],
-        include_dirs=[
-            'tools/include', os.path.join(relaxPath, 'MCFClass'),
-            os.path.join(relaxPath, 'OPTUtils'),
-            os.path.join(relaxPath, 'RelaxIV')])
-
-    setup(name='ARIAtools',
-          version=version0,
-          description='This is the ARIA tools package with RelaxIV support',
-          ext_modules=[module1],
-          packages=['ARIAtools'],
-          package_dir={'': 'tools'},
-          scripts=['tools/bin/ariaPlot.py', 'tools/bin/ariaDownload.py',
-                   'tools/bin/ariaExtract.py', 'tools/bin/ariaTSsetup.py',
-                   'tools/bin/ariaAOIassist.py', 'tools/bin/ariaMisclosure.py'
-                   'tools/bin/export_product.py'])
-else:
-    # Third party package RelaxIV not found
-    print('Installing ARIA-tools without support for RelaxIV')
-
-    setup(name='ARIAtools',
-          version=version0,
-          description='This is the ARIA tools package without RelaxIV support',
-          packages=['ARIAtools'],
-          package_dir={'': 'tools'},
-          scripts=['tools/bin/ariaPlot.py', 'tools/bin/ariaDownload.py',
-                   'tools/bin/ariaExtract.py', 'tools/bin/ariaTSsetup.py',
-                   'tools/bin/ariaAOIassist.py', 'tools/bin/ariaMisclosure.py',
-                   'tools/bin/export_product.py'])
+setup(name='ARIAtools',
+      version=version0,
+      description='Installing ARIA-tools',
+      packages=['ARIAtools'],
+      package_dir={'': 'tools'},
+      scripts=['tools/bin/ariaPlot.py', 'tools/bin/ariaDownload.py',
+               'tools/bin/ariaExtract.py', 'tools/bin/ariaTSsetup.py',
+               'tools/bin/ariaAOIassist.py', 'tools/bin/ariaMisclosure.py',
+               'tools/bin/export_product.py'])
