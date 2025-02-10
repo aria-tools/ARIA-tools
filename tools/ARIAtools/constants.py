d74164d78731aed531e52b4c8c031a7e0c29953b
Emre Havazli <emre.havazli@jpl.nasa.gov>
2025-02-10 11:27:43 -0800

    edit ARIA_TROPO_INTERNAL list

--- tools/ARIAtools/constants.py
+++ tools/ARIAtools/constants.py
@@ -22,8 +22,7 @@ ARIA_EXTERNAL_CORRECTIONS = ['troposphereHydrostatic',
 ARIA_INTERNAL_CORRECTIONS = ['ionosphere']
 
 ARIA_TROPO_INTERNAL = ['ERA5',
-                       'GMAO',
-                       'HRES',
+                    #    'HRES', # HRES model currently not available
                        'HRRR']
 
 ARIA_TROPO_MODELS = ARIA_TROPO_INTERNAL
