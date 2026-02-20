6070443a558a5ef4ff59b65e3a19ca32748d661a
Simran S Sangha <sssangha@ucla.edu>
2026-02-20 02:33:08 -0800

    Modify bperp data reading to ignore NaN values
    Updated the data reading process to use np.nanmean to ignore NaN values.

--- tools/ARIAtools/extractProduct.py
+++ tools/ARIAtools/extractProduct.py
@@ -1047,8 +1047,8 @@ def extract_bperp_dict(products, num_threads):
         # 1. Open explicitly
         ds = osgeo.gdal.Open(frame, osgeo.gdal.GA_ReadOnly)
         
-        # 2. Read data
-        res = ds.ReadAsArray().mean()
+        # 2. Read data and take mean, but ignore NaN values
+        res = np.nanmean(arr)
         
         # 3. CRITICAL: Close the file explicitly
         ds = None 
