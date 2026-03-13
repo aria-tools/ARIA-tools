421353fc630e9f24efb67ead67a1a020219350e4
Simran S Sangha <sssangha@ucla.edu>
2026-03-13 16:29:43 -0700

    Fix nagging benign memory error which clutters progress

--- tools/bin/ariaTSsetup.py
+++ tools/bin/ariaTSsetup.py
@@ -14,6 +14,7 @@ analysis. Specifically, extract unwrapped interferogram, coherence, perp
 baseline, LOS file(s), and (where available) tropospheric correction layers.
 """
 import os
+import sys
 import glob
 import copy
 import logging
@@ -737,6 +738,15 @@ def main():
             msg = f'Available layers are: {list(ARIA_STACK_OUTFILES.keys())}'
             LOGGER.warning(
                 'Selected layer %s not supported in tsSetup' + msg, layer)
+
+    # Flush standard streams and force a hard exit for the gnu_parallel worker.
+    # This completely bypasses Python's noisy GDAL C-binding garbage collection phase.
+    try:
+        sys.stdout.flush()
+        sys.stderr.flush()
+        os._exit(0)
+    except Exception:
+        pass
 
 
 if __name__ == '__main__':
