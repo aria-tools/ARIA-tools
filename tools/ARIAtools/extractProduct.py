c8e16de24bdafd54848cd27de4f232c944064b9f
Emre Havazli <emre.havazli@jpl.nasa.gov>
2026-05-12 16:21:30 -0700

    Avoid process-pool export for single-layer single-job extraction runs

--- tools/ARIAtools/extractProduct.py
+++ tools/ARIAtools/extractProduct.py
@@ -1590,10 +1590,12 @@ def _run_export_products_with_processes(mp_args, num_workers, layer):
 def _run_export_jobs(mp_args, num_workers, layer, multiproc_method):
     """Dispatch export jobs to the supported serial or process backends."""
 
-    if multiproc_method == 'single' or num_workers == 1:
-        if multiproc_method == 'processes' and num_workers == 1:
+    if multiproc_method == 'single' or num_workers == 1 or len(mp_args) <= 1:
+        if multiproc_method == 'processes' and (
+                num_workers == 1 or len(mp_args) <= 1):
             LOGGER.debug(
-                'Running %d total jobs serially because worker count is 1',
+                'Running %d total jobs serially because parallel export '
+                'would not provide any benefit',
                 len(mp_args),
             )
 
@@ -1612,7 +1614,8 @@ def _run_export_jobs(mp_args, num_workers, layer, multiproc_method):
 
     if multiproc_method == 'processes':
         LOGGER.debug('Running %d total jobs with processes', len(mp_args))
-        return _run_export_products_with_processes(mp_args, num_workers, layer)
+        return _run_export_products_with_processes(
+            mp_args, min(num_workers, len(mp_args)), layer)
 
     raise ValueError(
         f'Unknown multiproc_method "{multiproc_method}". Expected '
